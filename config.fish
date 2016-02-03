2f73dfc6777be9e99afabc0bc02d2214da80a471
Michal Hozza <mhozza@gmail.com>
2016-02-03 18:36:52 +0100

    added autojump and fzf

--- config.fish
+++ config.fish
@@ -1,6 +1,7 @@
 eval (python -m virtualfish compat_aliases)
 __fish_complete_django django-admin.py
 __fish_complete_django manage.py
+source /usr/share/autojump/autojump.fish
 
 set fish_color_host '-o'  'yellow'
 set -x PATH ~/scripts $PATH
---
+++ functions/fzf_key_bindings.fish
@@ -0,0 +1,1 @@
+/home/michal/.fzf/shell/key-bindings.fish
