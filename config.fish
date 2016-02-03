9a2f9c850d13d7e007efa53ccdf028dfdcbd9513
Michal Hozza <mhozza@gmail.com>
2016-02-03 17:46:25 +0100

    initial fish config

---
+++ config.fish
@@ -0,0 +1,6 @@
+eval (python -m virtualfish compat_aliases)
+__fish_complete_django django-admin.py
+__fish_complete_django manage.py
+
+set fish_color_host '-o'  'yellow'
+set -x PATH ~/scripts $PATH
